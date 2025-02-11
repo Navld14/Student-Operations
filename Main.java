e0ca345a44c7387f73bdf8cf140488733665b6c7
Navld14 <naveen.ladha.btech2023@sitpune.edu.in>
2025-02-11 11:58:03 +0530

    Added Main class with main method and initial setup

--- Main.java
+++ Main.java
@@ -1,1 +1,9 @@
+//Main.java
 
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        StudentOps operations = new StudentOps();
+    }
+}    
