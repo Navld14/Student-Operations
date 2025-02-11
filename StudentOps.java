49334b8a310f6c50dd7dca54016fd3264e5cd61e
Navld14 <naveen.ladha.btech2023@sitpune.edu.in>
2025-02-11 12:09:19 +0530

    Added addStudents method

--- StudentOps.java
+++ StudentOps.java
@@ -2,10 +2,15 @@
 import java.util.* ; 
 
 class StudentOps{
-	
 	ArrayList<Student> students;
     
+    //Create a constructor for StudentOps
     public StudentOps(){
         this.students = new ArrayList<>();
+    }
+    //Create addStudent method
+	public void addStudents(Student student){
+        students.add(student);
+        System.out.println("Student Added Successfully");
     }
 }
