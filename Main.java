4bb24d1c4ce882dc12066bbbd5d3b1b6e79fdc86
Navld14 <naveen.ladha.btech2023@sitpune.edu.in>
2025-02-11 11:58:24 +0530

    Implemented menu loop for user interaction

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
 //Main.java
 
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         StudentOps operations = new StudentOps();
     }
-}    
+    while (true) {
+            System.out.println("Choose an option:");
+            System.out.println("1. Add Student\n2. Display Students\n3. Update Student\n4. Remove Student\n5. Exit");
+            int choice = Integer.parseInt(scan.nextLine());
+    }
+}   
