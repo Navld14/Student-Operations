2e598d7cd9fee655c290f77d0f7d30d6c492cb2b
Navld14 <naveen.ladha.btech2023@sitpune.edu.in>
2025-02-11 12:09:54 +0530

    Create displayStudents method

--- StudentOps.java
+++ StudentOps.java
@@ -3,7 +3,6 @@ import java.util.* ;
 
 class StudentOps{
 	ArrayList<Student> students;
-    
     //Create a constructor for StudentOps
     public StudentOps(){
         this.students = new ArrayList<>();
@@ -12,5 +11,11 @@ class StudentOps{
 	public void addStudents(Student student){
         students.add(student);
         System.out.println("Student Added Successfully");
+    }
+    //Create a dispay method
+	public void displayStudents(){
+        for (Student student : students){
+            student.display();
+        }
     }
 }
