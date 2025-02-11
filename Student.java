8954954011f4de7043588bba2c628a461a209e59
Navld14 <naveen.ladha.btech2023@sitpune.edu.in>
2025-02-11 12:04:23 +0530

    Create a Student class constructor

--- Student.java
+++ Student.java
@@ -1,10 +1,17 @@
 //Student.java
 class Student{
-    
     //Setting private fields for Name, PRN, GPA, Batch, Branch
     private String name;
     private long prn;
     private double gpa;
     private String batch;
     private String branch;
+    //Create a Student class constructor
+    public Student(String name, long prn, double gpa, String batch, String branch) {
+        this.name = name;
+        this.prn = prn;
+        this.gpa = gpa;
+        this.branch = branch;
+        this.batch = batch;
+    }
 }
